1fe0b23eec60fa84f7ec4a78e117ca9f1058419a
Evans Mwangangi <emwangangi@webtribe.co.ke>
2022-11-17 00:05:46 +0300

    'crypto-currency

--- src/app/coin-list/coin-list.component.ts
+++ src/app/coin-list/coin-list.component.ts
@@ -1,15 +1,30 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../service/api.service';
 
 @Component({
   selector: 'app-coin-list',
   templateUrl: './coin-list.component.html',
-  styleUrls: ['./coin-list.component.css']
+  styleUrls: ['./coin-list.component.css'],
 })
 export class CoinListComponent implements OnInit {
+  bannerData: any = [];
 
-  constructor() { }
+  constructor(private api: ApiService) {}
 
   ngOnInit(): void {
+    this.getBannerData();
+    this.getAllData();
   }
 
+  getBannerData() {
+    this.api.getTrendingCurrency('INR').subscribe((res) => {
+      this.bannerData = res;
+      // console.log(res);
+    });
+  }
+  getAllData() {
+    this.api.getCurrencyData('INR').subscribe((res) => {
+      console.log(res);
+    });
+  }
 }
